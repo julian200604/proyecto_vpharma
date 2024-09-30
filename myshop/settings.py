bf0d139e5f7bdc063f225904ff363621d7322c76
Héctor Julián Guillén Urrea <128424495+julian200604@users.noreply.github.com>
2024-09-30 11:47:10 -0500

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = 'django-insecure-k6!#q&!j%-om)i^-0p!%-#2k3_kjtg*@p#lx3@xu@&0i474+x5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.234.251.16', 'localhost']
+#'18.234.251.16', 'localhost'
 
 
 # Application definition
@@ -74,12 +75,21 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'myshop_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'USER': 'admin',
+        'PASSWORD': 'julian2006-04',
+        'HOST': 'database-1.c1sc2k4a0g3o.us-east-1.rds.amazonaws.com',
         'PORT': '3306',
     }
 }
+
+#'default': {
+  #      'ENGINE': 'django.db.backends.mysql',
+  #      'NAME': 'myshop_db',
+    #    'USER': 'admin',
+    #    'PASSWORD': 'julian2006-04',
+     #   'HOST': 'database-1.c1sc2k4a0g3o.us-east-1.rds.amazonaws.com',
+    #    'PORT': '3306',
+  #  }
 
 
 # Password validation
