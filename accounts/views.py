19307a5f40b73a34067bbf5ffdf5f77c26bc972d
AndresQueved0 <sebastian_ramirezro@soy.sena.edu.co>
2024-10-01 21:43:36 -0500

    se creo el perfil y falta error

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from .models import Usuario, Rol
 from django.urls import reverse
 from .forms import RegistroForm
+from django.http import HttpResponseRedirect
 
 def register(request):
     if request.method == 'POST':
@@ -17,9 +18,15 @@ def register(request):
             # Crea el objeto Usuario relacionado
             Usuario.objects.create(user=user, rol=rol)
 
+            # Autentica al usuario
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
             # Mensaje de éxito
             messages.success(request, 'Registro exitoso. Ahora puedes iniciar sesión.')
-            return redirect('login')
+            return HttpResponseRedirect(reverse('shop:product_list'))
         else:
             # Mensaje de error con detalles de los errores del formulario
             for field in form:
@@ -40,14 +47,14 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 request.session['logged_in'] = True
-                return redirect('perfil')
+                return HttpResponseRedirect(reverse('shop:product_list'))
             else:
                 messages.error(request, 'Nombre de usuario o contraseña incorrectos')
         else:
             messages.error(request, 'Error al autenticar el usuario')
     else:
         if request.session.get('logged_in'):
-            return redirect('tienda')
+            return HttpResponseRedirect(reverse('shop:product_list'))
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
 
@@ -57,8 +64,16 @@ def logout_view(request):
     return redirect('shop:product_list')
 
 def perfil(request):
-    request.session['logged_in'] = True
-    if request.user.is_superuser:
-        return redirect('admin:index')
+    if request.user.is_authenticated:
+        perfil = request.user.perfil
+        if request.method == 'POST':
+            form = RegistroForm(request.POST, instance=perfil)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Perfil actualizado correctamente')
+                return redirect('perfil')
+        else:
+            form = RegistroForm(instance=perfil)
+        return render(request, 'accounts/perfil.html', {'form': form})
     else:
-        return render(request, 'accounts/perfil.html')
+        return redirect('login')
