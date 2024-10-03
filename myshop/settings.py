00f0602706f97c0bbaa341b5553dbde4b5b386fe
Héctor Julián Guillén Urrea <128424495+julian200604@users.noreply.github.com>
2024-10-03 18:58:42 -0500

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-k6!#q&!j%-om)i^-0p!%-#2k3_kjtg*@p#lx3@xu@&0i474+x5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.234.251.16', 'localhost]
+ALLOWED_HOSTS = ['18.234.251.16', 'localhost']
 #'18.234.251.16', 'localhost'
 
 
